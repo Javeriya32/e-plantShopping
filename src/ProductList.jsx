36cdb7e12c1ea266eee46b2f6156ed0beac1bb4a
Javeriya32 <hakeemjaveriya16@gmail.com>
2026-01-19 09:17:09 -0500

    Display plant list with add to cart button

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -274,6 +274,32 @@ function ProductList({ onHomeClick }) {
             </div>
             {!showCart ? (
                 <div className="product-grid">
+                    {plantsArray.map((category, index) => (
+  <div key={index}>
+    <h1>{category.category}</h1>
+    <div className="product-list">
+      {category.plants.map((plant, plantIndex) => (
+        <div className="product-card" key={plantIndex}>
+          <img
+            src={plant.image}
+            alt={plant.name}
+            className="product-image"
+          />
+          <div className="product-title">{plant.name}</div>
+          <div className="product-description">{plant.description}</div>
+          <div className="product-cost">${plant.cost}</div>
+          <button
+            className="product-button"
+            onClick={() => handleAddToCart(plant)}
+          >
+            Add to Cart
+          </button>
+        </div>
+      ))}
+    </div>
+  </div>
+))}
+
 
 
                 </div>
